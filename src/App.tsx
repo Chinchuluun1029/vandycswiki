b35693e35b19ba544977b4e998974bafdff43fba
Chinchuluun Munkh-Achit <chinchuluunmunkh-achit@Chinchuluuns-MacBook-Pro.local>
2020-10-26 15:46:23 -0500

    adding 404 route and minimizing lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route } from "wouter";
-
+import { Route, Switch } from "wouter";
 
 import Header from './comp/Header/Header';
 import Footer from './comp/Footer/Footer';
@@ -9,29 +8,20 @@ import Landing from './comp/Landing/Landing';
 import Classes from './comp/Classes/Classes';
 import Graph from './comp/Graph/Graph';
 
-import LINKS from "./constants/links";
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Route path="/">
-        <Landing />
-      </Route>
-      <Route path="/home">
-        <Landing />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/classes">
-        <Classes />
-      </Route>
-      <Route path="/graph">
-        <Graph />
-      </Route>
+      <Switch>
+        <Route path="/"><Landing /></Route>
+        <Route path="/home"><Landing /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/classes"><Classes /></Route>
+        <Route path="/graph"><Graph /></Route>
+        <Route>404, not found</Route>
+      </Switch>
       <Footer />
     </div>
   );
